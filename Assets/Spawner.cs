91199e41beae97c9b1e4cdefb566519d1e7c2ef0
Ment <netwig@mail.hs-ulm.de>
2020-09-30 12:00:44 +0200

    first commit

---
+++ Assets/Spawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject[] groups;
+
+    void SpawnNext()
+    {
+        int rand = Random.Range(0, groups.Length);
+
+        Instantiate(groups[rand], transform.position, Quaternion.identity);
+    }
+
+    void Start()
+    {
+        SpawnNext();
+    }
+}
